4a96001c4dfea386f98f0b8c13ee626713d03e34
Mickey <micksheridan.24@gmail.com>
2019-04-06 14:48:08 -0400

    submit

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,31 @@
-class CrewMember {}
+let crewIds = 0;
+const cantina = [];
+class CrewMember {
+  constructor(position, ship = "Looking for a rig") {
+    this.position = position;
+    this.currentShip = ship;
+    this.id = ++crewIds;
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect";
+    } else if (this.position === "Pilot") {
+      this.currentShip.warpDrive = "engaged";
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect";
+    } else if (this.position === "Defender") {
+      this.currentShip.cloaked = true;
+    }
+  }
+  chargePhasers() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect";
+    } else if (this.position === "Gunner") {
+      this.currentShip.phasersCharge = "charged";
+    }
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+let shipIds = 0;
+const ships = [];
+
+class Spaceship {
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged", phasersCharge = "uncharged") {
+    this.name = name;
+    this.crew = crew;
+    this.crew.forEach(member => {
+      member.currentShip = this;
+    });
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = cloaked;
+    this.warpDrive = warpDrive;
+    this.docked = this.crew.length === 0;
+    this.phasersCharge = phasersCharge;
+    ships.push(this);
+    this.id = ++shipIds;
+  }
+}
